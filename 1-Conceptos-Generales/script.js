50f97b9e5a9e4cc7d5b31ac51c705118dfb133e8
FranciscoPugh <franciscopugh01@gmail.com>
2021-10-25 12:33:04 -0300

    Clase 01

---
+++ 1-Conceptos-Generales/script.js
@@ -0,0 +1,30 @@
+/*
+var nombre = "Francisco Pugh"
+let nombre1 = "Milton Salazar"
+
+console.log(nombre)
+console.log(nombre1)
+
+var nombre = "Cesar Pagliacci"
+console.log(nombre)
+nombre1 = "Anabella Pippo"
+console.log(nombre1)
+
+var nombre = 10
+console.log(nombre)
+
+*/
+
+const IVA = 1.21
+
+let numero1 = parseInt(prompt("Ingresa un numero"))
+let numero2 = parseFloat(prompt("Ingrese otro numero"))
+
+let nombre1 = "Fran"
+let nombre2 = "Pancho"
+
+alert(nombre1 + " " + nombre2)
+document.write(`<h1> ${nombre1} ${nombre2} </h1>`)
+console.log(numero1 + numero2)
+
+//IVA = 1.30
